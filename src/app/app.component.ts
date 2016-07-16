1104ebdf6ae7d2d9517d524dcf50c64ad2306943
Philip Orchard <orchardp@ohsu.edu>
2016-07-16 22:15:16 +0000

    Add movies component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MoviesComponent} from './components/movies/movies.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [MoviesComponent]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'app is working!';
 }
